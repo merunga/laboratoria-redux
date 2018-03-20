96ce69ab4252019438c4ea1c5eba7de2dcc8a601
merunga <merunga@gmail.com>
2018-03-20 08:18:57 -0500

    configuracion redux

--- ranking/src/index.js
+++ ranking/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css'
-import App from './App'
+import App from './components/App'
+import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker()
